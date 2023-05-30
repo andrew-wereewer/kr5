a34ebe39e53707610d1ce99cbcbaa8d7c3ad944c
wereewer <wereewergt@gmail.com>
2023-05-30 18:47:34 +0300

    рассылка оповещений о статьях за неделю

--- NewsPaper/news/management/commands/runapscheduler.py
+++ NewsPaper/news/management/commands/runapscheduler.py
@@ -1,12 +1,36 @@
 import logging
+from django.core.mail import EmailMultiAlternatives
 from apscheduler.schedulers.blocking import BlockingScheduler
 from apscheduler.triggers.cron import CronTrigger
 from django.core.management.base import BaseCommand
 from django.conf import settings
+from django.db.models import Q
 from django_apscheduler.jobstores import DjangoJobStore
+from news.models import (Post, Subscription)
+from datetime import datetime, timedelta
 
 def send_spam():
-  logging.getLogger(__name__).debug('ТИК')
+  new_posts = Post.objects.filter(~Q(isnews=1), created__gt=datetime.today()-timedelta(days=7)).prefetch_related('categories')
+  #[print(p) for p in new_posts]
+  #[[print(f'post:{q}, category:{d}') for d in q.categories.all()] for q in new_posts]
+  categories = new_posts.values('categories').distinct().filter(categories__isnull=False)
+  #[print(f) for f in categories]
+  subscriptions = Subscription.objects.filter(category__in=categories.all()).select_related('user')
+  #[print(u) for u in subscriptions.all()]
+  #print('\n====sending=====')
+  [send_letter(sub, list(filter(None, [p if sub.category.id in [pc.id for pc in p.categories.all()] else None for p in new_posts]))) for sub in subscriptions]
+
+def send_letter(subscription, posts):
+  #print(f'\ncategory: {subscription.category.name}({subscription.category.id})')
+  #print(f'user: {subscription.user.username}({subscription.user.email})')
+  #[print(f"post: {p}") for p in posts]
+  text_content = '\n'.join([f'Автор: {p.author.user.username}, Название: {p.title}, Опубликокана: {p.created.strftime("%d/%m/%Y")}' for p in posts])
+  html_content = '<ul><li>'+'</li><li>'.join([f'Автор: {p.author.user.username}, Название: {p.title}, Опубликокана: {p.created.strftime("%d/%m/%Y")}</li>' for p in posts]) + '</li></ul>'
+  msg = EmailMultiAlternatives(
+    f'Статьи в категории {subscription.category.name} за последнюю неделю',
+     text_content, None, [subscription.user.email])
+  msg.attach_alternative(html_content, "text/html")
+  msg.send()
 
 class Command(BaseCommand):
 
@@ -18,7 +42,7 @@ class Command(BaseCommand):
 
     scheduler.add_job(
       send_spam,
-      trigger=CronTrigger(second=0), #(day_of_week="fri", hour="18"),
+      trigger=CronTrigger(day_of_week="fri", hour="18"),
       id="send_spam",
       max_instances=1,
       replace_existing=True,
